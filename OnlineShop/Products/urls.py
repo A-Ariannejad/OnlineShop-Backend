68013541ad2ea1f20b2ce9a33039b30e05fb7b56
A_Ariannejad <ariannejad14@gmail.com>
2023-06-23 19:25:30 +0430

    categories and search is added

--- OnlineShop/Products/urls.py
+++ OnlineShop/Products/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ProductCreateView, ProductViewSet, ProductDeleteView, ProductUpdateView
+from .views import ProductCreateView, ProductViewSet, ProductDeleteView, ProductUpdateView, ProductShowView, ProductSearchView, ProductCategoriesView
 
 urlpatterns = [
     path('list/', ProductViewSet.as_view({'get': 'list'}), name='list'),
+    path('show/<int:id>/', ProductShowView.as_view(), name='show'),
     path('create/<int:pk>/', ProductCreateView.as_view(), name='create'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete'),
     path('update/<int:pk>/', ProductUpdateView.as_view(), name='update'),
+    path('search/', ProductSearchView.as_view(), name='search'),
+    path('categories/', ProductCategoriesView.as_view(), name='categories-search'),
 ]
--- OnlineShop/Products/views.py
+++ OnlineShop/Products/views.py
@@ -1,9 +1,15 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, filters
 from Users.views import IsUser, IsAdmin
 from Stores.models import Store
 from .models import Product
 from .serializer import ProductCreateSerializer, ProductSerializer
 
+
+class ProductShowView(generics.RetrieveAPIView):
+    permission_classes = [IsUser]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -29,3 +35,24 @@ class ProductUpdateView(generics.UpdateAPIView):
     permission_classes = [IsUser]
     queryset = Product.objects.all()
     serializer_class = ProductCreateSerializer
+
+class ProductSearchView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'type', 'bio']
+
+class ProductCategoriesView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        L_price_input = self.request.query_params.get('lower_price', 0)
+        U_price_input = self.request.query_params.get('upper_price', 99999999999999999)
+        type_input = self.request.query_params.get('type', None)
+        if type_input:
+            queryset = queryset.filter(type__icontains=type_input)
+        queryset = queryset.filter(price__range=(L_price_input, U_price_input))
+        return queryset
