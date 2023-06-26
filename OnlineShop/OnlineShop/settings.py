b9a1ba7f28f689913f2fcd5bd33921ee9c977245
A_Ariannejad <ariannejad14@gmail.com>
2023-06-26 20:00:29 +0430

    updated

--- OnlineShop/OnlineShop/settings.py
+++ OnlineShop/OnlineShop/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 AUTH_USER_MODEL = 'Users.CustomUser'
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000','http://127.0.0.1:3000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000','http://127.0.0.1:3000','https://online-shop-frontend.vercel.app/']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_PRIVATE_NETWORK = True
